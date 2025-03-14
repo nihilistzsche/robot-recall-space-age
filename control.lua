b2dd3e35f26cb0d659d2a453feeba346703b79af
Nihilistzsche <nihilistzsche@gmail.com>
2025-03-14 00:57:34 -0700

    Factorio 2.0 update

--- control.lua
+++ control.lua
@@ -1,63 +1,15 @@
+rr = {}
 -- require('util')
-require('control.robot-recall')
-require('control.robot-redistribute')
+require("control.robot-recall")
+require("control.robot-redistribute")
 -- require('')
 
 script.on_init(function(event)
-    global.teleportQueue = {}
-    global.teleportQueueEntryCount = 0
-    global.hasChanged = false
-    global.openedGUIPlayers = {}
-    global.deploying = {}
+	storage.teleportQueue = {}
+	storage.teleportQueueEntryCount = 0
+	storage.hasChanged = false
+	storage.openedGUIPlayers = {}
+	storage.deploying = {}
 end)
 
-script.on_configuration_changed(function(event)
-        if (event.mod_changes and event.mod_changes['robot-recall']) then
-        local old_ver = event.mod_changes['robot-recall'].old_version
-        if (old_ver) then
-            if (old_ver == "0.2.0" or string.find(old_ver, "0.1.")) then
-                global.teleportQueue = {}
-                global.teleportQueueEntryCount = 0
-            end
-
-            if (old_ver == "0.2.0" or old_ver == "0.2.1") then
-                global.deploying = {}
-                for _, surface in pairs(game.surfaces) do
-                    local deploymentstations =
-                        surface.find_entities_filtered(
-                            {name = "robot-redistribute-chest"})
-                    for k, v in pairs(deploymentstations) do
-                        global.deploying[v.unit_number] =
-                            {ent = v, deploying = false}
-                    end
-                end
-            end
-            if (new_ver == "0.2.1" or 
-                new_ver == "0.2.2" or 
-                new_ver == "0.2.3" or 
-                string.find(old_ver, "0.1.")) then 
-                local newTeleportQueue = {}
-                for k,e in pairs(global.teleportQueue) do
-                    local newEl = e
-                    if (newEl.source and newEl.source.valid) then
-                        newEl.srcPos = newEl.source.position
-                        newEl.surface = newEl.source.surface
-                    end
-                    if (newEl.destination and newEl.destination.valid) then
-                        newEl.surface = newEl.destination.surface
-                        newEl.destPos = newEl.destination.position
-                    end
-                    table.insert(newTeleportQueue, newEl)
-                end
-                global.teleportQueue = newTeleportQueue
-                global.teleportQueueEntryCount = table_size(newTeleportQueue)
-            end
-        end
-    end
-
-    global.deploying = global.deploying or {}
-    global.teleportQueue = global.teleportQueue or {}
-    global.teleportQueueEntryCount = global.teleportQueueEntryCount or 0
-    global.hasChanged = global.hasChanged or false
-    global.openedGUIPlayers = global.openedGUIPlayers or {}
-end)
+script.on_configuration_changed(function(event) end)
--- control/robot-redistribute.lua
+++ control/robot-redistribute.lua
@@ -1,50 +1,46 @@
 script.on_nth_tick(15, function(event)
-    for k, v in pairs(global.deploying) do
-        if (v.deploying and v.ent.valid) then
-            local inv = v.ent.get_inventory(1)
-            if (not inv.is_empty()) then
-                local items = inv.get_contents()
-                for name, count in pairs(items) do
-                    local placeresult = game.item_prototypes[name].place_result
-                    if (placeresult.type == "logistic-robot" or placeresult.type ==
-                        "construction-robot") then
-                        if (logisticNetworkHasItemSpace(v.ent.logistic_network, name)) then
-                            v.ent.surface.create_entity(
-                                {
-                                    name = placeresult.name,
-                                    position = v.ent.position,
-                                    force = v.ent.logistic_network.force
-                                })
-                            inv.remove({name = name, count = 1})
-                        end
-                    end
-
-                end
-            end
-        end
-    end
+	for k, v in pairs(storage.deploying) do
+		if v.deploying and v.ent.valid then
+			local inv = v.ent.get_inventory(defines.inventory.item_main)
+			if not inv.is_empty() then
+				local items = inv.get_contents()
+				for _, info in pairs(items) do
+					local placeresult = prototypes.item[info.name].place_result
+					if
+						placeresult
+						and (placeresult.type == "logistic-robot" or placeresult.type == "construction-robot")
+					then
+						if logisticNetworkHasItemSpace(v.ent.logistic_network, info.name) then
+							v.ent.surface.create_entity({
+								name = placeresult.name,
+								position = v.ent.position,
+								force = v.ent.logistic_network.force,
+							})
+							inv.remove({ name = info.name, count = 1 })
+						end
+					end
+				end
+			end
+		end
+	end
 end)
 
-script.on_event(defines.events.on_built_entity, function(event) 
-
-    if (event.created_entity.name == "robot-redistribute-chest") then
-        local entity = event.created_entity
-        global.deploying[entity.unit_number] = {ent = entity, deploying = false}
-    end   
-
+script.on_event(defines.events.on_built_entity, function(event)
+	if event.entity.name == "robot-redistribute-chest" then
+		local entity = event.entity
+		storage.deploying[entity.unit_number] = { ent = entity, deploying = false }
+	end
 end)
 
-script.on_event(defines.events.on_robot_built_entity, function(event) 
-
-    if (event.created_entity.name == "robot-redistribute-chest") then
-        local entity = event.created_entity
-        global.deploying[entity.unit_number] = {ent = entity, deploying = false}
-    end   
-
+script.on_event(defines.events.on_robot_built_entity, function(event)
+	if event.entity.name == "robot-redistribute-chest" then
+		local entity = event.entity
+		storage.deploying[entity.unit_number] = { ent = entity, deploying = false }
+	end
 end)
 
 -- function canInsertIntoRoboport(itemname, logistic_network)
---     -- local roboport = 
+--     -- local roboport =
 --     if (logistic_network and logistic_network.valid) then
 --         for k, v in pairs(logistic_network.cells) do
 --             local inv = cell.owner.get_inventory(
@@ -58,22 +54,25 @@ end)
 -- end
 
 function logisticNetworkHasItemSpace(logistic_network, itemname)
-    if (logistic_network and logistic_network.valid) then
-        for k, v in pairs(logistic_network.cells) do
-            local inv = v.owner.get_inventory(defines.inventory.roboport_robot)
-            if (inv and inv.can_insert({name = itemname, count = 1})) then
-                return true
-            end
-        end
-    end
-    return false
+	if logistic_network and logistic_network.valid then
+		for k, v in pairs(logistic_network.cells) do
+			local inv = v.owner.get_inventory(defines.inventory.roboport_robot)
+			if inv and inv.can_insert({ name = itemname, count = 1 }) then
+				return true
+			end
+		end
+	end
+	return false
 end
 
 script.on_nth_tick(60, function(event)
-    for k, v in pairs(global.deploying) do
-        if (v.ent and not v.ent.valid) then table.remove(global.deploying, k) 
-        elseif (v.ent and not v.ent.get_inventory(1).is_empty()) then v.deploying = true 
-        else v.deploying = false end
-    end
-
+	for k, v in pairs(storage.deploying) do
+		if v.ent and not v.ent.valid then
+			table.remove(storage.deploying, k)
+		elseif v.ent and not v.ent.get_inventory(1).is_empty() then
+			v.deploying = true
+		else
+			v.deploying = false
+		end
+	end
 end)
